b645f35d1752dc9389dc008d44dbbdfa15576b4a
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-06-12 16:45:18 -0500

    add RCV for selected actor, movie and director

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,18 @@ class PagesController < ApplicationController
   end
 
   def movies
-    @dmovies_record = Movie.all
+    @movies_record = Movie.all
   end
 
   def actors
     @actors_record = Actor.all
+  end
+  def selected_actor
+    actor_id = params[:actor_id]
+    @actor = Actor.where(id: actor_id).first
+  end
+  def selected_director
+  end
+  def selected_movie
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get "directors/youngest", controller: "most", action: "youngest"
-  get "directors/oldest", controller: "most", action: "oldest"
+  get "directors/eldest", controller: "most", action: "eldest"
   get "/directors", controller: "pages", action: "directors"
   get "/movies", controller: "pages", action: "movies"
   get "/actors", controller: "pages", action: "actors"
-  get("/", { controller: "misc", action: "homepage" })
+  get "/", controller: "misc", action: "homepage"
+  get "/actors/:actor_id", controller: "pages", action: "selected_actor"
+  get "/directors/:director_id", controller: "pages", action: "selected_director"
+  get "/movies/:movie_id", controller: "pages", action: "selected_movie"
 end
